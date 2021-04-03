533618a6dff105986c703c38f9b313cd75ea4a1f
Mercedes <mvazquezsan@gmail.com>
2021-04-03 23:29:49 +0200

    node updated

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const handlebars = require('handlebars');
 const exphbs = require('express-handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 const bodyparser = require('body-parser');
+
+var app = express();
+
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Potentially Hazardous Asteroids Website');
+});
+
+app.listen(3000, () => {
+    console.log('connected 2 DB');
+});
